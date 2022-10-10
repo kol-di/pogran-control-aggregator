c39ffbc8fb20d2105a38c1487416df2fb7b8e351
kol-di <60890747+kol-di@users.noreply.github.com>
2022-10-10 17:57:57 +0500

    Small data structure changes

--- PKparser/parser.py
+++ PKparser/parser.py
@@ -3,7 +3,7 @@ from cleantext import clean
 from string import ascii_lowercase
 
 
-def parse_locations(text):
+def parse_tags(text):
     locations = []
 
     sent_pattern = re.compile(r"#(.*?)($|\.)", re.DOTALL)
@@ -12,10 +12,8 @@ def parse_locations(text):
     hashtag_pattern = re.compile(r"(?<=#)(.*?)(?=\s)")
     for match in matches:
         hashtags = re.findall(hashtag_pattern, clean(match.group(0), to_ascii=False, no_emoji=True))
-        loc = {tag: True for tag in hashtags}
-        if 'отказ' not in hashtags:
-            loc['отказ'] = False
-        locations.append(loc)
+        refuse_and_locs = ('отказ' in hashtags, hashtags)
+        locations.append(refuse_and_locs)
 
     return locations
 
@@ -30,7 +28,7 @@ def get_msg_chunk(message):
     serialized_msg = dict(filter(lambda el: el[0] in fields, message.to_dict().items()))
     try:
         msg_id, msg_txt, msg_date = [serialized_msg[key] for key in fields]
-        location_tags = parse_locations(msg_txt)
+        location_tags = parse_tags(msg_txt)
         msg_chunk = [{
             '_id': str(msg_id) + loc_id,
             'date': msg_date,
@@ -38,8 +36,8 @@ def get_msg_chunk(message):
             'tags': tags}
             for loc_id, refused, tags in zip(
                 ascii_lowercase[:len(location_tags)],
-                map(lambda x: x['отказ'], location_tags),
-                map(lambda x: (x, x.pop('отказ'))[0], location_tags))]
+                map(lambda x: x[0], location_tags),
+                map(lambda x: x[1], location_tags))]
     except KeyError:
         pass
 
